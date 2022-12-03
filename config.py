4136ac72729731c8a0284356b9f9df87f3b17ca5
Hanihere <115630779+hanihere@users.noreply.github.com>
2022-12-03 14:07:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = (os.environ.get("APP_ID", ""))
 
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = (os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = (os.environ.get("OWNER_ID", ""))
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
@@ -23,7 +23,7 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = (os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
